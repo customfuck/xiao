6376b1f259b350465c819cd555b9202191d8ad53
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-06-03 19:21:03 +0530

    Create ganyu.py

---
+++ zerotwobot/modules/ganyu.py
@@ -0,0 +1,17 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from zerotwobot.events import register
+from zerotwobot import telethn as tbot
+
+
+PHOTO = "yaha par tgm dalna ganyu ka "
+
+@register(pattern=("/ganyu"))
+async def awake(event):
+  TEXT = f"**CONSTELLATION** \n\n"
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
