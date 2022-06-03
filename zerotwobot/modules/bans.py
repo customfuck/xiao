25052367a9d0030f2435ad4cbb7eec4c2923ae8d
Axel <86648749+Axelrexkun@users.noreply.github.com>
2022-06-04 01:21:57 +0530

    Update bans.py

--- zerotwobot/modules/bans.py
+++ zerotwobot/modules/bans.py
@@ -110,7 +110,7 @@ def ban(update: Update, context: CallbackContext) -> str:
     
         if is_user_ban_protected(chat, user_id, member) and user not in DEV_USERS:
             if user_id == OWNER_ID:
-                message.reply_text("Trying to put me against a my master huh? know ur place")
+                message.reply_text("Trying to put me against my master huh? know ur place")
             elif user_id in DEV_USERS:
                 message.reply_text("I can't act against our own void members.")
             elif user_id in DRAGONS:
