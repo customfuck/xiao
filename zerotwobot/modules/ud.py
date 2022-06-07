13b96573137804085742872bc48525907919e630
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-06-07 20:55:03 +0530

    Update ud.py

--- zerotwobot/modules/ud.py
+++ zerotwobot/modules/ud.py
@@ -5,22 +5,22 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
 
-def ud(update: Update, context: CallbackContext):
+def data(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text[len("/ud ") :]
     results = requests.get(
-        f"https://api.urbandictionary.com/v0/define?term={text}",
+        f"https://pokemondb.net/pokedex/{text}",
     ).json()
     try:
-        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+        reply_text = f'*{text}*\n\n{results["BASE STATS"][0]["Moves learnt and Tms"]}\n\n_{results["BASE STATS"][0]["Moves learnt and base"]}_'
     except:
         reply_text = "No results found."
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
-UD_HANDLER = DisableAbleCommandHandler(["ud"], ud, run_async=True)
+DATA_HANDLER = DisableAbleCommandHandler(["data"], data, run_async=True)
 
-dispatcher.add_handler(UD_HANDLER)
+dispatcher.add_handler(Data_HANDLER)
 
 __command_list__ = ["ud"]
-__handlers__ = [UD_HANDLER]
+__handlers__ = [DATA_HANDLER]
